c408f31569e0fdd0d2693d9d6c4fd45edfe87035
exer1221 <everperez0721@gmail.com>
2025-06-07 11:18:33 -0500

    Se implemento interactividad en PestañaPrincipal y sus vistas asociadas -Se conecto navegacion entre catalogo, detalles, carrito, pasarela de pago y lista de deseados -Se configuro botones y transiciones entre ventanas en PestañaPrincipal_vista.fxml -Aun no se implementa el registro de productos en lista de deseados ni en el carrito

--- OlevStore/src/controlador/PestañaPrincipal_vistaController.java
+++ OlevStore/src/controlador/PestañaPrincipal_vistaController.java
@@ -1,20 +1,15 @@
 package controlador;
 
-import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class PestañaPrincipal_vistaController implements Initializable {
@@ -51,31 +46,92 @@ public class PestañaPrincipal_vistaController implements Initializable {
     private Pane pnDescMouseLogitech;
     @FXML
     private Button btnAñadirAlCarrito11;
-    
+
     private Stage stage;
+
+    @FXML
+    private Text textoTitular;
+    @FXML
+    private Pane paneCarritoCompras;
+    @FXML
+    private Pane paneListaDeseados;
+    @FXML
+    private Pane pnPasarelaPago;
+    @FXML
+    private Pane confNequi;
+    @FXML
+    private Pane confPaypal;
+    @FXML
+    private Pane confMasterCard;
+    @FXML
+    private Pane pnPagoMasterCard;
+    @FXML
+    private Pane pnPagoNequiPaypal;
 
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }  
-    
-    public void setStage(Stage stage){
+        pnBtnCarritoCompra.setVisible(true);
+        pnBtnListaDeseados.setVisible(true);
+        paneBarrasBusqueda.setVisible(true);
+
+        pnBtnInicio.setVisible(false);
+        textoTitular.setVisible(false);
+
+        //Siempre tengo que iniciar en el catalogo (true) y las demas dejarlas false, por seguridad xD
+        ventanasFalse();
+
+        paneCatalogo.setVisible(true);
+        paneCatalogo.toFront();
+
+    }
+
+    public void setStage(Stage stage) {
         this.stage = stage;
     }
-    
+
+    public void ventanasFalse() {
+        pnDescControlXBOX.setVisible(false);
+        pnDescMouseAttack.setVisible(false);
+        pnDescMouseLogitech.setVisible(false);
+        paneCarritoCompras.setVisible(false);
+        paneListaDeseados.setVisible(false);
+
+        pnPasarelaPago.setVisible(false);
+    }
+
     public void closeWindow() {
         stage.close();
     }
 
     @FXML
     private void btnCarritoCompra_Clicked(MouseEvent event) {
+        paneCatalogo.setVisible(false);
+        pnBtnCarritoCompra.setVisible(false);
+        pnBtnListaDeseados.setVisible(false);
+        pnBtnInicio.setVisible(true);
+        textoTitular.setVisible(true);
+        textoTitular.setText("Carrito De Compras");
+
+        //Ventana
+        paneCarritoCompras.setVisible(true);
+
+        paneBarrasBusqueda.setVisible(false);
     }
 
     @FXML
     private void btnListaDeseados_Clicked(MouseEvent event) {
+        paneCatalogo.setVisible(false);
+        paneBarrasBusqueda.setVisible(false);
+        textoTitular.setVisible(true);
+        textoTitular.setText("Lista De Deseos");
+        paneListaDeseados.setVisible(true);
+
+        pnBtnInicio.setVisible(true);
+        pnBtnCarritoCompra.setVisible(false);
+        pnBtnListaDeseados.setVisible(false);
     }
 
     @FXML
@@ -84,6 +140,112 @@ public class PestañaPrincipal_vistaController implements Initializable {
 
     @FXML
     private void btnInicio_Clicked(MouseEvent event) {
+
+        //Creo que con este metodo es mejor (Que flojera estar cambiandole el estado a todo de nuevo)
+        ventanasFalse();
+
+        paneCatalogo.setVisible(true);
+
+        pnBtnInicio.setVisible(false);
+        pnBtnCarritoCompra.setVisible(true);
+        pnBtnListaDeseados.setVisible(true);
+
+        textoTitular.setVisible(false);
+        paneBarrasBusqueda.setVisible(true);
+    }
+
+    @FXML
+    private void btnControlXBOX_Clicked(MouseEvent event) {
+        paneCatalogo.setVisible(false);
+        pnDescControlXBOX.setVisible(true);
+
+        pnBtnCarritoCompra.setVisible(false);
+        pnBtnListaDeseados.setVisible(false);
+        pnBtnInicio.setVisible(true);
+    }
+
+    @FXML
+    private void btnMouseAttack_Clicked(MouseEvent event) {
+        paneCatalogo.setVisible(false);
+        pnDescMouseAttack.setVisible(true);
+
+        pnBtnCarritoCompra.setVisible(false);
+        pnBtnListaDeseados.setVisible(false);
+        pnBtnInicio.setVisible(true);
+    }
+
+    @FXML
+    private void btnMouseLogitech(MouseEvent event) {
+        paneCatalogo.setVisible(false);
+        pnDescMouseLogitech.setVisible(true);
+
+        pnBtnCarritoCompra.setVisible(false);
+        pnBtnListaDeseados.setVisible(false);
+        pnBtnInicio.setVisible(true);
+    }
+
+    @FXML
+    private void btnNequi_Clicked(MouseEvent event) {
+        cerrarMetodoPago();
+        
+        confNequi.setVisible(true);
+        confMasterCard.setVisible(false);
+        confPaypal.setVisible(false);
+
+        pnPagoNequiPaypal.setVisible(true);
+    }
+
+    @FXML
+    private void btnPaypal_Click(MouseEvent event) {
+        cerrarMetodoPago();
+        
+        confNequi.setVisible(false);
+        confMasterCard.setVisible(false);
+        confPaypal.setVisible(true);
+
+        pnPagoNequiPaypal.setVisible(true);
+    }
+
+    @FXML
+    private void btnMasterCard_Clicked(MouseEvent event) {
+        cerrarMetodoPago();
+        
+        confNequi.setVisible(false);
+        confMasterCard.setVisible(true);
+        confPaypal.setVisible(false);
+
+        pnPagoMasterCard.setVisible(true);
+    }
+
+    @FXML
+    private void btnPagar_Clicked(MouseEvent event) {
+        abrirPasarelaPago();
+    }
+
+    @FXML
+    private void btnPagarTodo_Clicked(MouseEvent event) {
+        abrirPasarelaPago();
     }
     
+    public void cerrarMetodoPago() {
+        pnPagoNequiPaypal.setVisible(false);
+        pnPagoMasterCard.setVisible(false);
+    }
+
+    public void abrirPasarelaPago() {
+        paneCarritoCompras.setVisible(false);
+        pnPasarelaPago.setVisible(true);
+        textoTitular.setVisible(true);
+        textoTitular.setText("Pasarela De Pago");
+
+        //Hice esto para no tener que repetir mucho codigo, se ve feo :v
+        cerrarMetodoPago();
+        
+        confNequi.setVisible(false);
+        confMasterCard.setVisible(false);
+        confPaypal.setVisible(false);
+
+        
+    }
+
 }
